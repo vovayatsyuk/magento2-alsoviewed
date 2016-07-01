c04c385f7d89e25335b452128d1139f2bf019e56
Vova Yatsyuk <vova.yatsyuk@gmail.com>
2016-07-01 21:25:39 +0300

    Relation table added. #1

--- Setup/InstallSchema.php
+++ Setup/InstallSchema.php
@@ -42,6 +42,104 @@ class InstallSchema implements InstallSchemaInterface
 
         $setup->getConnection()->createTable($table);
 
+        /**
+         * Create table 'alsoviewed_relation'
+         */
+        $table = $setup->getConnection()
+            ->newTable($setup->getTable('alsoviewed_relation'))
+            ->addColumn(
+                'relation_id',
+                \Magento\Framework\DB\Ddl\Table::TYPE_INTEGER,
+                null,
+                ['identity' => true, 'nullable' => false, 'unsigned' => true, 'primary' => true],
+                'Relation ID'
+            )
+            ->addColumn(
+                'product_id',
+                \Magento\Framework\DB\Ddl\Table::TYPE_INTEGER,
+                null,
+                ['unsigned' => true, 'nullable' => false],
+                'Product ID'
+            )
+            ->addColumn(
+                'related_product_id',
+                \Magento\Framework\DB\Ddl\Table::TYPE_INTEGER,
+                null,
+                ['unsigned' => true, 'nullable' => false],
+                'Related Product ID'
+            )
+            ->addColumn(
+                'weight',
+                \Magento\Framework\DB\Ddl\Table::TYPE_INTEGER,
+                null,
+                ['unsigned' => true, 'nullable' => false, 'default'  => 0],
+                'Relation Weight'
+            )
+            ->addColumn(
+                'position',
+                \Magento\Framework\DB\Ddl\Table::TYPE_INTEGER,
+                null,
+                ['nullable' => false, 'default'  => 50],
+                'Custom Sort Order'
+            )
+            ->addColumn(
+                'status',
+                \Magento\Framework\DB\Ddl\Table::TYPE_SMALLINT,
+                null,
+                ['unsigned' => true, 'nullable' => false, 'default'  => 1],
+                'Status'
+            )
+            ->addColumn(
+                'updated_at',
+                \Magento\Framework\DB\Ddl\Table::TYPE_TIMESTAMP,
+                null,
+                ['nullable' => false, 'default' => \Magento\Framework\DB\Ddl\Table::TIMESTAMP_INIT_UPDATE],
+                'Update Date'
+            )
+            ->addIndex(
+                $setup->getIdxName(
+                    'alsoviewed_relation',
+                    ['product_id', 'related_product_id'],
+                    \Magento\Framework\DB\Adapter\AdapterInterface::INDEX_TYPE_UNIQUE
+                ),
+                ['product_id', 'related_product_id'],
+                ['type' => \Magento\Framework\DB\Adapter\AdapterInterface::INDEX_TYPE_UNIQUE]
+            )
+            ->addIndex(
+                $setup->getIdxName('alsoviewed_relation', ['product_id']),
+                ['product_id']
+            )
+            ->addIndex(
+                $setup->getIdxName('alsoviewed_relation', ['weight', 'position']),
+                ['weight', 'position']
+            )
+            ->addIndex(
+                $setup->getIdxName('alsoviewed_relation', ['status']),
+                ['status']
+            )
+            ->addIndex(
+                $setup->getIdxName('alsoviewed_relation', ['updated_at']),
+                ['updated_at']
+            )
+            ->addForeignKey(
+                $setup->getFkName('alsoviewed_relation', 'product_id', 'catalog_product_entity', 'entity_id'),
+                'product_id',
+                $setup->getTable('catalog_product_entity'),
+                'entity_id',
+                \Magento\Framework\DB\Ddl\Table::ACTION_CASCADE,
+                \Magento\Framework\DB\Ddl\Table::ACTION_CASCADE
+            )
+            ->addForeignKey(
+                $setup->getFkName('alsoviewed_relation', 'related_product_id', 'catalog_product_entity', 'entity_id'),
+                'related_product_id',
+                $setup->getTable('catalog_product_entity'),
+                'entity_id',
+                \Magento\Framework\DB\Ddl\Table::ACTION_CASCADE,
+                \Magento\Framework\DB\Ddl\Table::ACTION_CASCADE
+            )
+            ->setComment('Alsoviewed Log Table');
+        $setup->getConnection()->createTable($table);
+
         $setup->endSetup();
     }
 }
--- Setup/UpgradeSchema.php
+++ Setup/UpgradeSchema.php
@@ -20,6 +20,10 @@ class UpgradeSchema implements UpgradeSchemaInterface
             $this->dropForeignKeysFromLogTable($setup);
         }
 
+        if (version_compare($context->getVersion(), '0.1.0', '<')) {
+            $this->createRelationTable($setup);
+        }
+
         $setup->endSetup();
     }
 
@@ -79,5 +83,104 @@ class UpgradeSchema implements UpgradeSchemaInterface
             $setup->getConnection()->dropForeignKey($table, $key);
             $setup->getConnection()->dropIndex($table, $key);
         }
+    }
+
+    public function createRelationTable(SchemaSetupInterface $setup)
+    {
+        $table = $setup->getConnection()
+            ->newTable($setup->getTable('alsoviewed_relation'))
+            ->addColumn(
+                'relation_id',
+                \Magento\Framework\DB\Ddl\Table::TYPE_INTEGER,
+                null,
+                ['identity' => true, 'nullable' => false, 'unsigned' => true, 'primary' => true],
+                'Relation ID'
+            )
+            ->addColumn(
+                'product_id',
+                \Magento\Framework\DB\Ddl\Table::TYPE_INTEGER,
+                null,
+                ['unsigned' => true, 'nullable' => false],
+                'Product ID'
+            )
+            ->addColumn(
+                'related_product_id',
+                \Magento\Framework\DB\Ddl\Table::TYPE_INTEGER,
+                null,
+                ['unsigned' => true, 'nullable' => false],
+                'Related Product ID'
+            )
+            ->addColumn(
+                'weight',
+                \Magento\Framework\DB\Ddl\Table::TYPE_INTEGER,
+                null,
+                ['unsigned' => true, 'nullable' => false, 'default'  => 0],
+                'Relation Weight'
+            )
+            ->addColumn(
+                'position',
+                \Magento\Framework\DB\Ddl\Table::TYPE_INTEGER,
+                null,
+                ['nullable' => false, 'default'  => 50],
+                'Custom Sort Order'
+            )
+            ->addColumn(
+                'status',
+                \Magento\Framework\DB\Ddl\Table::TYPE_SMALLINT,
+                null,
+                ['unsigned' => true, 'nullable' => false, 'default'  => 1],
+                'Status'
+            )
+            ->addColumn(
+                'updated_at',
+                \Magento\Framework\DB\Ddl\Table::TYPE_TIMESTAMP,
+                null,
+                ['nullable' => false, 'default' => \Magento\Framework\DB\Ddl\Table::TIMESTAMP_INIT_UPDATE],
+                'Update Date'
+            )
+            ->addIndex(
+                $setup->getIdxName(
+                    'alsoviewed_relation',
+                    ['product_id', 'related_product_id'],
+                    \Magento\Framework\DB\Adapter\AdapterInterface::INDEX_TYPE_UNIQUE
+                ),
+                ['product_id', 'related_product_id'],
+                ['type' => \Magento\Framework\DB\Adapter\AdapterInterface::INDEX_TYPE_UNIQUE]
+            )
+            ->addIndex(
+                $setup->getIdxName('alsoviewed_relation', ['product_id']),
+                ['product_id']
+            )
+            ->addIndex(
+                $setup->getIdxName('alsoviewed_relation', ['weight', 'position']),
+                ['weight', 'position']
+            )
+            ->addIndex(
+                $setup->getIdxName('alsoviewed_relation', ['status']),
+                ['status']
+            )
+            ->addIndex(
+                $setup->getIdxName('alsoviewed_relation', ['updated_at']),
+                ['updated_at']
+            )
+            ->addForeignKey(
+                $setup->getFkName('alsoviewed_relation', 'product_id', 'catalog_product_entity', 'entity_id'),
+                'product_id',
+                $setup->getTable('catalog_product_entity'),
+                'entity_id',
+                \Magento\Framework\DB\Ddl\Table::ACTION_CASCADE,
+                \Magento\Framework\DB\Ddl\Table::ACTION_CASCADE
+            )
+            ->addForeignKey(
+                $setup->getFkName('alsoviewed_relation', 'related_product_id', 'catalog_product_entity', 'entity_id'),
+                'related_product_id',
+                $setup->getTable('catalog_product_entity'),
+                'entity_id',
+                \Magento\Framework\DB\Ddl\Table::ACTION_CASCADE,
+                \Magento\Framework\DB\Ddl\Table::ACTION_CASCADE
+            )
+            ->setComment('Alsoviewed Log Table');
+
+        $setup->getConnection()->createTable($table);
     }
 }

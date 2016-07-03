cae20b6e6cb2fea6bdea59abe0f31c20789b73b8
Vova Yatsyuk <vova.yatsyuk@gmail.com>
2016-07-03 14:46:36 +0300

    Added 'massDelete' action. #1

---
+++ Controller/Adminhtml/Relation/MassDelete.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Vovayatsyuk\Alsoviewed\Controller\Adminhtml\Relation;
+
+use Magento\Framework\Controller\ResultFactory;
+use Magento\Backend\App\Action\Context;
+use Magento\Ui\Component\MassAction\Filter;
+use Vovayatsyuk\Alsoviewed\Model\ResourceModel\Relation\CollectionFactory;
+
+class MassDelete extends \Magento\Backend\App\Action
+{
+    /**
+     * Authorization level of a basic admin session
+     *
+     * @see _isAllowed()
+     */
+    const ADMIN_RESOURCE = 'Vovayatsyuk_Alsoviewed::relation';
+
+    /**
+     * Massactions filter
+     *
+     * @var Filter
+     */
+    protected $filter;
+
+    /**
+     * @var CollectionFactory
+     */
+    protected $collectionFactory;
+
+    /**
+     * @param Context $context
+     * @param Filter $filter
+     * @param CollectionFactory $collectionFactory
+     */
+    public function __construct(
+        Context $context,
+        Filter $filter,
+        CollectionFactory $collectionFactory
+    ) {
+        $this->filter = $filter;
+        $this->collectionFactory = $collectionFactory;
+        parent::__construct($context);
+    }
+
+    /**
+     * @return \Magento\Backend\Model\View\Result\Redirect
+     */
+    public function execute()
+    {
+        $collection = $this->filter->getCollection($this->collectionFactory->create());
+        $collectionSize = $collection->getSize();
+
+        foreach ($collection->getItems() as $relation) {
+            $relation->delete();
+        }
+
+        $this->messageManager->addSuccess(
+            __('A total of %1 record(s) have been deleted.', $collectionSize)
+        );
+
+        /** @var \Magento\Backend\Model\View\Result\Redirect $resultRedirect */
+        $resultRedirect = $this->resultFactory->create(ResultFactory::TYPE_REDIRECT);
+        return $resultRedirect->setPath('*/*/');
+    }
+}
--- Model/ResourceModel/Relation/Collection.php
+++ Model/ResourceModel/Relation/Collection.php
@@ -4,6 +4,11 @@ namespace Vovayatsyuk\Alsoviewed\Model\ResourceModel\Relation;
 
 class Collection extends \Vovayatsyuk\Alsoviewed\Model\ResourceModel\Collection\AbstractCollection
 {
+    /**
+     * @var string
+     */
+    protected $_idFieldName = 'relation_id';
+
     protected function _construct()
     {
         $this->_init(

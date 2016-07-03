ff65d217634d1e48e58b3d430b9cca712911e5db
Vova Yatsyuk <vova.yatsyuk@gmail.com>
2016-07-03 21:07:23 +0300

    MassStatus action added

---
+++ Controller/Adminhtml/Relation/MassStatus.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Vovayatsyuk\Alsoviewed\Controller\Adminhtml\Relation;
+
+use Magento\Framework\Controller\ResultFactory;
+use Magento\Backend\App\Action\Context;
+use Magento\Ui\Component\MassAction\Filter;
+use Vovayatsyuk\Alsoviewed\Model\ResourceModel\Relation\CollectionFactory;
+
+class MassStatus extends \Magento\Backend\App\Action
+{
+    /**
+     * Authorization level of a basic admin session
+     *
+     * @see _isAllowed()
+     */
+    const ADMIN_RESOURCE = 'Vovayatsyuk_Alsoviewed::relation';
+
+    /**
+     * MassActions filter
+     *
+     * @var Filter
+     */
+    protected $filter;
+
+    /**
+     * @var CollectionFactory
+     */
+    protected $collectionFactory;
+
+    /**
+     * @param Context $context
+     * @param Filter $filter
+     * @param CollectionFactory $collectionFactory
+     */
+    public function __construct(
+        Context $context,
+        Filter $filter,
+        CollectionFactory $collectionFactory
+    ) {
+        $this->filter = $filter;
+        $this->collectionFactory = $collectionFactory;
+        parent::__construct($context);
+    }
+
+    /**
+     * @return \Magento\Backend\Model\View\Result\Redirect
+     * @throws \Magento\Framework\Exception\LocalizedException|\Exception
+     */
+    public function execute()
+    {
+        $collection = $this->filter->getCollection($this->collectionFactory->create());
+        $status = (int) $this->getRequest()->getParam('status');
+
+        foreach ($collection as $item) {
+            $item->setStatus($status);
+            $item->save();
+        }
+
+        $this->messageManager->addSuccess(
+            __('A total of %1 record(s) have been updated.', $collection->getSize())
+        );
+
+        /** @var \Magento\Backend\Model\View\Result\Redirect $resultRedirect */
+        $resultRedirect = $this->resultFactory->create(ResultFactory::TYPE_REDIRECT);
+        return $resultRedirect->setPath('*/*/');
+    }
+}

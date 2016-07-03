36d3501e1dda98ed7969464ac732305483e4fe02
Vova Yatsyuk <vova.yatsyuk@gmail.com>
2016-07-03 22:24:55 +0300

    Added cronjob to process log every 15 minutes. #1

---
+++ Cron/ProcessLog.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Vovayatsyuk\Alsoviewed\Cron;
+
+use Vovayatsyuk\Alsoviewed\Model\ResourceModel\Log;
+use Vovayatsyuk\Alsoviewed\Model\ResourceModel\Relation;
+
+class ProcessLog
+{
+    /**
+     * @var \Vovayatsyuk\Alsoviewed\Model\ResourceModel\Log
+     */
+    protected $log;
+
+    /**
+     * @var \Vovayatsyuk\Alsoviewed\Model\ResourceModel\Relation
+     */
+    protected $relation;
+
+    /**
+     * @var \Psr\Log\LoggerInterface
+     */
+    protected $logger;
+
+    /**
+     * @param Context $context
+     * @param Log $log
+     * @param Relation $relation
+     */
+    public function __construct(
+        Log $log,
+        Relation $relation,
+        \Psr\Log\LoggerInterface $logger
+    )
+    {
+        $this->log = $log;
+        $this->relation = $relation;
+        $this->logger = $logger;
+    }
+
+    public function execute()
+    {
+        $data = $this->log->getGroupedRelations();
+        if ($data) {
+            try {
+                $this->relation->updateRelations($data);
+                $this->log->clean();
+            } catch (\Exception $e) {
+                $this->logger->critical(
+                    sprintf(
+                        "Alsoviewed 'ProcessLog' Cron Event exception: %s",
+                        $e->getMessage()
+                    )
+                );
+            }
+        }
+    }
+}
